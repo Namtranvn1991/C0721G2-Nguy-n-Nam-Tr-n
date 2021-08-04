6e269e78a0b8ad6e90c4e8c2d4ac1b507b0e433f
Namtranvn1991 <namtranvn1991@gmail.com>
2021-08-04 17:08:10 +0700

    1

---
+++ module1/6. Tổng quan JavaScript/thuchanh/nhungJavaScript.js
@@ -0,0 +1,17 @@
+var A;
+function a() {
+    alert("hello!")
+}
+document.getElementById("123").innerHTML = "xin chao!";
+function b() {
+    document.write("654321")
+}
+function type1() {
+    A=prompt("yeu cau nhap")
+}
+function type2() {
+    console.log(A)
+}
+function type3() {
+    document.getElementById("hienthinhap").innerText = A
+}
