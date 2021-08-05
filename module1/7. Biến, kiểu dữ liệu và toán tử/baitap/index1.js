8be7a725b4653422b0276e4375b3f5979b91017a
Namtranvn1991 <namtranvn1991@gmail.com>
2021-08-05 16:14:47 +0700

    1

--- module1/7. Biến, kiểu dữ liệu và toán tử/baitap/index1.js
+++ module1/7. Biến, kiểu dữ liệu và toán tử/baitap/index1.js
@@ -9,21 +9,21 @@ function convert() {
     if (amount<0){alert("nhập lại giá trị lớn hơn 0")};
     if (from=="VND"&&to=="USD"){
         result=amount/20000;
-        document.getElementById("result").innerText = result.toLocaleString() + "USD";}
-    else if (from=="VND"&&to=="JPY"){
+        document.getElementById("result").innerText = result.toLocaleString() + "USD";
+    } else if (from=="VND"&&to=="JPY"){
         result=amount/200;
-        document.getElementById("result").innerText = result.toLocaleString() + "JPY";}
-    else if (from=="USD"&&to=="VND"){
+        document.getElementById("result").innerText = result.toLocaleString() + "JPY";
+    } else if (from=="USD"&&to=="VND"){
         result=amount*20000;
-        document.getElementById("result").innerText = result.toLocaleString() + "VND";}
-    else if (from=="USD"&&to=="JPY"){
+        document.getElementById("result").innerText = result.toLocaleString() + "VND";
+    } else if (from=="USD"&&to=="JPY"){
         result=amount*200;
-        document.getElementById("result").innerText = result.toLocaleString() + "JPY";}
-    else if (from=="JPY"&&to=="USD"){
+        document.getElementById("result").innerText = result.toLocaleString() + "JPY";
+    } else if (from=="JPY"&&to=="USD"){
         result=amount/200;
-        document.getElementById("result").innerText = result.toLocaleString() + "USD";}
-    else if (from=="JPY"&&to=="VND"){
+        document.getElementById("result").innerText = result.toLocaleString() + "USD";
+    } else if (from=="JPY"&&to=="VND"){
         result=amount*200;
-        document.getElementById("result").innerText = result.toLocaleString() + "VND";}
-    else (alert("Trùng đơn vị"));
+        document.getElementById("result").innerText = result.toLocaleString() + "VND";
+    } else {alert("Trùng đơn vị")};
 }
