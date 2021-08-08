47a37bee6e305851dac1ef827824109dd2758df2
Namtranvn1991 <namtranvn1991@gmail.com>
2021-08-08 14:22:33 +0700

    1

--- module1/Baitapthem/index4.js
+++ module1/Baitapthem/index4.js
@@ -1,5 +1,16 @@
-let parent1 = document.getElementById("parent1");
-function delete1() {
-    let child1 = document.getElementById("title1");
-    parent1.removeChild(child1);
-}
+function delete1 () {
+    var title1= document.getElementById("title1");
+    title1.remove();
+};
+function delete2 () {
+    var title2= document.getElementById("title2");
+    title2.remove();
+};
+function delete3 () {
+    var title3= document.getElementById("title3");
+    title3.remove();
+};
+function delete4 () {
+    var title4= document.getElementById("title4");
+    title4.remove();
+};
--- module1/Baitapthem/index6.js
+++ module1/Baitapthem/index6.js
@@ -1,4 +1,14 @@
 function result() {
-    let myName= document.getElementById("myName").value
-    let habit
+    let myName = document.getElementById("myName").value;
+    let hobby = document.getElementsByName("hobby");
+    let kqua = "";
+    for ( let i = 0; i < hobby.length; i++) {
+        if (hobby[i].checked===true){
+            kqua += "["+hobby[i].value+"]";
+        }
+    }
+    alert("Sở thích của " + myName + " là " + kqua);
+    let qqq = document.getElementById("myName")
+    let qq = qqq.innerHTML;
+    console.log(qq);
 }
