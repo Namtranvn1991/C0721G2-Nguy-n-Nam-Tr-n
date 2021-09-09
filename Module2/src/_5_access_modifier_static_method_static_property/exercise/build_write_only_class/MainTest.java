3d984aa88f16cfc24ac11d37eeaef230e3130acd
Namtranvn1991 <namtranvn1991@gmail.com>
2021-09-09 21:06:46 +0700

    9/9

--- Module2/src/_5_access_modifier_static_method_static_property/exercise/build_write_only_class/MainTest.java
+++ Module2/src/_5_access_modifier_static_method_static_property/exercise/build_write_only_class/MainTest.java
@@ -1,6 +1,6 @@
 package _5_access_modifier_static_method_static_property.exercise.build_write_only_class;
 
-public class mainTest {
+public class MainTest {
     public static void main(String[] args) {
         Student student = new Student();
         student.setName("Tran");
