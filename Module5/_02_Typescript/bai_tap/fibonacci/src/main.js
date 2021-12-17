9af0c8182430dc3c07009aedb49d4be3d6a390bb
Namtranvn1991 <namtranvn1991@gmail.com>
2021-12-17 10:37:53 +0700

    17/12

--- Module5/_02_Typescript/bai_tap/fibonacci/src/main.js
+++ Module5/_02_Typescript/bai_tap/fibonacci/src/main.js
@@ -1,12 +1,19 @@
-let fibonacci = [0, 1];
+var fibonacci = [0, 1];
 function listFibonacci(num) {
-    // starting at array index 1, and push current index + previous index to the array
     for (var i = 1; i < num; i++) {
         fibonacci.push(fibonacci[i] + fibonacci[i - 1]);
     }
     console.log(fibonacci);
     return fibonacci;
 }
-let total = 0;
+var total = 0;
 listFibonacci(5).forEach(function (value) { return total += value; });
 console.log(total);
+function Fibonacci(n) {
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+var number = 3;
+console.log(Fibonacci(number));
