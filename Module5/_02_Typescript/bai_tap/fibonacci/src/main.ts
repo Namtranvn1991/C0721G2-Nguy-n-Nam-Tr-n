abe989dac77eca581693499fd5bfd1a030b851e4
Namtranvn1991 <namtranvn1991@gmail.com>
2021-12-17 10:35:46 +0700

    17/12

--- Module5/_02_Typescript/bai_tap/fibonacci/src/main.ts
+++ Module5/_02_Typescript/bai_tap/fibonacci/src/main.ts
@@ -1,7 +1,6 @@
 let fibonacci = [0,1];
 
 function listFibonacci(num:number) {
-    // starting at array index 1, and push current index + previous index to the array
     for (let i = 1; i < num; i++) {
         fibonacci.push(fibonacci[i] + fibonacci[i - 1]);
     }
@@ -13,3 +12,14 @@ let total = 0;
 listFibonacci(5).forEach(value => total+=value);
 console.log(total)
 
+
+
+function Fibonacci(n:number):number {
+    if(n ==1 || n ==2){
+        return  1;
+    }
+    return Fibonacci(n-1) + Fibonacci(n-2);
+
+}
+let number: number = 3;
+console.log(Fibonacci(number));
